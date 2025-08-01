61571eef5dcf79e99dad3f35d750e02cd67be06b
kuch <obangoriemi1@gmail.com>
2025-08-01 21:18:19 +0300

    update pitch video

--- script.js
+++ script.js
@@ -36,3 +36,21 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
         localStorage.theme = 'light';
     }
   }
+
+
+const btnEl = document.querySelector(".btn")
+const closeIconEl = document.querySelector(".close-icon")
+const trailerContainerEl = document.querySelector(".trailer-container")
+const videoEl = document.querySelector("video")
+
+
+btnEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.remove("active")
+})
+
+closeIconEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.add("active")
+    videoEl.pause()
+    videoEl.currentTime = 0
+    
+})
